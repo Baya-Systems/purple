bd74c9b877d9d36921132307103fdf2d2072bb0e
James Aldis <james.aldis@bayasystems.com>
2026-08-27 16:41:25 +0100

    version number

--- src/purple/__about__.py
+++ src/purple/__about__.py
@@ -1,6 +1,6 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __required_python_version__ = "3.13"
 
 '''
-version numbering will be odd numbers for 3.13 and even numbers for 3.14
+version numbering will be odd numbers in last position for 3.13 and even numbers for 3.14
 '''
